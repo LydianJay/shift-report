bc4199fff5915870593124d4ea66c34e675a80aa
LydianJay <lloydjayedradan@gmail.com>
2025-03-02 23:46:57 +0800

    employee seeder

--- resources/views/components/basedashboard.blade.php
+++ resources/views/components/basedashboard.blade.php
@@ -42,15 +42,14 @@
         <script src="{{ asset('vendor/creative-tim/js/plugins/smooth-scrollbar.min.js') }}"></script>
         <script src="{{ asset('vendor/creative-tim/js/plugins/chartjs.min.js') }}"></script>
         <script src="{{ asset('vendor/creative-tim/js/plugins/swiper-bundle.min.js') }}" type="text/javascript"></script>
-        <!-- Control Center for Corporate UI Dashboard: parallax effects, scripts for the example pages etc -->
         <script src="{{ asset('vendor/creative-tim/js/corporate-ui-dashboard.min.js?v=1.0.0') }}"></script>
     </head>
     <body class="g-sidenav-show  bg-gray-100">
         <aside class="sidenav navbar navbar-vertical navbar-expand-xs border-0 bg-slate-900 fixed-start " id="sidenav-main">
             <div class="sidenav-header">
                 <i class="fas fa-times p-3 cursor-pointer text-secondary opacity-5 position-absolute end-0 top-0 d-none d-xl-none" aria-hidden="true" id="iconSidenav"></i>
-                <a class="navbar-brand d-flex align-items-center m-0" href=" https://demos.creative-tim.com/corporate-ui-dashboard/pages/dashboard.html " target="_blank">
-                <span class="font-weight-bold text-lg">Corporate UI</span>
+                <a class="navbar-brand d-flex align-items-center m-0" href="#" target="_blank">
+                <span class="font-weight-bold text-lg">{{ config('constants.title') }}</span>
                 </a>
             </div>
             <div class="collapse navbar-collapse px-4  w-auto " id="sidenav-collapse-main">
