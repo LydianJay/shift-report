28a3c74e5f5cd158ba9210f172011133b9b9c7a8
LydianJay <lloydjayedradan@gmail.com>
2025-03-04 09:49:58 +0800

    to be continued

--- config/routes.php
+++ config/routes.php
@@ -3,15 +3,24 @@
 return [
 
     'dashboard' => [
-        'name'  => 'dashboard',
-        'title' => 'Dashboard',
-        'icon'  => 'bi bi-grid-1x2',
-        'route' => 'dashboard',
+        'name'      => 'dashboard',
+        'title'     => 'Dashboard',
+        'icon'      => 'bi bi-grid-1x2',
+        'route'     => 'dashboard',
+        'auth_lvl'  => 2,
     ],
     'report' => [
-        'name'  => 'report',
-        'title' => 'Shift Report',
-        'icon'  => 'bi bi-table',
-        'route' => 'table',
+        'name'      => 'report',
+        'title'     => 'Shift Report',
+        'icon'      => 'bi bi-table',
+        'route'     => 'table',
+        'auth_lvl'  => 2,
+    ],
+    'employee' => [
+        'name'      => 'employee',
+        'title'     => 'Employee Management',
+        'icon'      => 'bi bi-person',
+        'route'     => 'employee',
+        'auth_lvl'  => 2,
     ],
 ];
--- resources/views/components/basedashboard.blade.php
+++ resources/views/components/basedashboard.blade.php
@@ -43,6 +43,87 @@
         <script src="{{ asset('vendor/creative-tim/js/plugins/chartjs.min.js') }}"></script>
         <script src="{{ asset('vendor/creative-tim/js/plugins/swiper-bundle.min.js') }}" type="text/javascript"></script>
         <script src="{{ asset('vendor/creative-tim/js/corporate-ui-dashboard.min.js?v=1.0.0') }}"></script>
+
+        <style>
+
+            :root {
+                --bs-blue: #63B3ED;
+                --bs-indigo: #596CFF;
+                --bs-purple: #6f42c1;
+                --bs-pink: #d63384;
+                --bs-red: #F56565;
+                --bs-orange: #fd7e14;
+                --bs-yellow: #FBD38D;
+                --bs-green: #81E6D9;
+                --bs-teal: #20c997;
+                --bs-cyan: #0dcaf0;
+                --bs-white: #fff;
+                --bs-gray: #6c757d;
+                --bs-gray-dark: #343a40;
+                --bs-gray-100: #f9fafb;
+                --bs-gray-200: #e9ecef;
+                --bs-gray-300: #dde0e5;
+                --bs-gray-400: #ced4da;
+                --bs-gray-500: #adb5bd;
+                --bs-gray-600: #6c757d;
+                --bs-gray-700: #495057;
+                --bs-gray-800: #343a40;
+                --bs-gray-900: #212529;
+                --bs-primary: #774dd3;
+                --bs-secondary: #64748b;
+                --bs-success: #67c23a;
+                --bs-info: #55a6f8;
+                --bs-warning: #f19937;
+                --bs-danger: #ea4e3d;
+                --bs-light: #cbd5e1;
+                --bs-dark: #1e293b;
+                --bs-white: #fff;
+                --bs-primary-rgb: 119, 77, 211;
+                --bs-secondary-rgb: 100, 116, 139;
+                --bs-success-rgb: 103, 194, 58;
+                --bs-info-rgb: 85, 166, 248;
+                --bs-warning-rgb: 241, 153, 55;
+                --bs-danger-rgb: 234, 78, 61;
+                --bs-light-rgb: 203, 213, 225;
+                --bs-dark-rgb: 30, 41, 59;
+                --bs-white-rgb: 255, 255, 255;
+                --bs-white-rgb: 255, 255, 255;
+                --bs-black-rgb: 0, 0, 0;
+                --bs-body-color-rgb: 100, 116, 139;
+                --bs-body-bg-rgb: 255, 255, 255;
+                --bs-font-sans-serif: "Noto Sans", "Open Sans";
+                --bs-font-monospace: SFMono-Regular, Menlo, Monaco, Consolas, "Liberation Mono", "Courier New", monospace;
+                --bs-gradient: linear-gradient(180deg, rgba(255, 255, 255, 0.15), rgba(255, 255, 255, 0));
+                --bs-body-font-family: var(--bs-font-sans-serif);
+                --bs-body-font-size: 1rem;
+                --bs-body-font-weight: 400;
+                --bs-body-line-height: 1.5;
+                --bs-body-color: #64748b;
+                
+                --bs-body-bg: #12101b;
+                --bs-border-width: 1px;
+                --bs-border-style: solid;
+                --bs-border-color: #dde0e5;
+                --bs-border-color-translucent: rgba(0, 0, 0, 0.175);
+                --bs-border-radius: 0.375rem;
+                --bs-border-radius-sm: 0.25rem;
+                --bs-border-radius-lg: 0.75rem;
+                --bs-border-radius-xl: 1rem;
+                --bs-border-radius-2xl: 1.5rem;
+                --bs-border-radius-pill: 50rem;
+                --bs-heading-color: #1e293b;
+                --bs-link-color: #774dd3;
+                --bs-link-hover-color: #522aaa;
+                --bs-code-color: #d63384;
+                --bs-highlight-bg: #fcf8e3;
+                }
+
+            .body {
+                
+                background-color: var(--bs-body-bg)
+            }
+        </style>
+
     </head>
     <body class="g-sidenav-show  bg-gray-100">
         <aside class="sidenav navbar navbar-vertical navbar-expand-xs border-0 bg-slate-900 fixed-start " id="sidenav-main">
@@ -56,12 +137,12 @@
                 <ul class="navbar-nav">
                     @foreach( config('routes') as $d)
 
-                        <li class="nav-item my-1">
-                            <a class="nav-link  active" href="../pages/dashboard.html">
+                        <li class="nav-item my-1 border border-secondary border-radius-md ">
+                            <a class="nav-link active" href="../pages/dashboard.html">
                                 <div class="icon icon-shape icon-sm shadow border-radius-md bg-white text-center me-2 d-flex align-items-center justify-content-center">
                                     <i class="{{$d['icon']}} fs-5 opacity-10 text-dark"></i>
                                 </div>
-                                <span class="nav-link-text ms-1">{{$d['title']}}</span>
+                                <span class="nav-link-text text-wrap">{{$d['title']}}</span>
                             </a>
                         </li>
                     
@@ -70,13 +151,13 @@
                 
             </div>
         </aside>
-        <main class="main-content position-relative max-height-vh-100 h-100 border-radius-lg ">
+        <main class="main-content position-relative max-height-vh-100 h-100 border-radius-lg bg-dark text-white">
             <!-- Navbar -->
             <nav class="navbar navbar-main navbar-expand-lg mx-5 px-0 shadow-none rounded mb-3" id="navbarBlur" navbar-scroll="true">
                 <div class="container-fluid py-1 px-2">
                     <nav aria-label="breadcrumb">
                         <ol class="breadcrumb bg-transparent mb-1 pb-0 pt-1 px-0 me-sm-6 me-5">
-                            <li class="breadcrumb-item text-sm"><a class="opacity-5 text-dark" href="javascript:;">Pages</a></li>
+                            <li class="breadcrumb-item text-sm"><a class="opacity-5 " href="javascript:;">Pages</a></li>
                             <li class="breadcrumb-item text-sm text-dark active" aria-current="page">Dashboard</li>
                         </ol>
                         <h6 class="font-weight-bold mb-0">Dashboard</h6>
